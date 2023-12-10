f8242531aea187dbb63c09c8d8ffd8f8ff89d83a
Nathan Develops <92740411+NathanDevelops1@users.noreply.github.com>
2023-12-10 11:11:54 -0500

    Create MainScreen.swift

---
+++ MainScreen.swift
@@ -0,0 +1,33 @@
+//
+//  MainScreen.swift
+//  Money Up
+//
+//  Created by Nathan Graff on 12/10/23.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    Text("Home")
+                        .font(.system(size: 30))
+                        .bold()
+                        .foregroundColor(Color.white)
+                    Spacer()
+                }
+                .padding(.bottom, 8)
+                .background(Color(red: 75/255, green: 160/255, blue: 108/255))
+                Spacer()
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+#Preview {
+    MainScreen()
+}
